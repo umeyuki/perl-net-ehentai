7caaf2d65dd201418d47c66ff565c339f21b4a10
umeyuki <umeyuki1326+github@gmail.com>
2013-03-23 09:41:00 +0900

    initial commit

---
+++ lib/Net/E/Hentai.pm
@@ -0,0 +1,32 @@
+package Net::E::Hentai;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Net::E::Hentai -
+
+=head1 SYNOPSIS
+
+  use Net::E::Hentai;
+
+=head1 DESCRIPTION
+
+Net::E::Hentai is
+
+=head1 AUTHOR
+
+umeyuki E<lt>umeyuki1326{at}gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
